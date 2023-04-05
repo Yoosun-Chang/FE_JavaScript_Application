062854cc77615c3cb6006566b68ee92e3643e138
sunny <ceusun0815@naver.com>
2023-04-05 20:15:35 +0900

    complete!

--- Problem5.js
+++ Problem5.js
@@ -4,3 +4,11 @@ let user = {
   name: "윤정",
   age: 20,
 };
+
+let json = JSON.stringify(user);
+alert(json);
+
+let userData = `{"name: "윤정", "age":23}`;
+let newuser = JSON.parse(json);
+
+alert(newuser);
